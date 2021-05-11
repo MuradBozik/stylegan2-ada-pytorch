3eef809f270e049ac492064dc9004af38832eb19
MuradBozik <murad.bozik@gmail.com>
2021-05-11 18:42:15 +0200

    generate updated

--- generate.py
+++ generate.py
@@ -112,14 +112,19 @@ def generate_images(
         if class_idx is not None:
             print ('warn: --class=lbl ignored when running on an unconditional network')
 
+    zs = list()
+
     # Generate images.
     for seed_idx, seed in enumerate(seeds):
         print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
         z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+        zs.append(z)
         img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
         PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
 
+    with open(f"{outdir}/latent_codes.npy", 'wb') as f:
+        np.save(f, np.array(zs))
 
 #----------------------------------------------------------------------------
 
